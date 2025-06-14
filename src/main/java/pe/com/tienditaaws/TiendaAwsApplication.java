efd7e5d46fe7d1f7c359460f5182586f9ba55f4f
gamma <santiagogamarrasanchez@gmail.com>
2025-06-14 08:37:25 -0500

    Rename Project

--- src/main/java/pe/com/tienditaaws/TiendaAwsApplication.java
+++ src/main/java/pe/com/tienditaaws/TiendaAwsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TienditaAwsApplication {
+public class TiendaAwsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TienditaAwsApplication.class, args);
+		SpringApplication.run(TiendaAwsApplication.class, args);
 	}
 
 }
